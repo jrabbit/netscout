86409634f1bff59b8bd81d5a472dd4367289c8d9
Jack Laxson <jackjrabbit@gmail.com>
2020-11-15 04:02:43 -0500

    working code

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## A tool to download and sync a list of tarballs and binaries of note/importance
 
 target: systemd timer and script
+
+
+## Deps 
+
+ - [uscan](apt://devscripts/)
+
--- netscout.zsh
+++ netscout.zsh
@@ -1,7 +1,23 @@
 #!/usr/bin/env zsh
 
-function dltime() {
-	wget -nc "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.23.tar.bz2"
+function madison() {
+	#wget -nc "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.23.tar.bz2"
+	for pkg ("signify-openbsd" "gnupg2") 
+		do newest=$(apt-cache madison $pkg | cut -d \| -f2 | xargs | cut -d " " -f 1)
+		echo $newest
+	done
 }
 
-dltime
+function usc() {
+	for pkg ("signify-openbsd" "gnupg2")
+		do uscanout=$(cd $pkg && uscan 2> /dev/null) 
+		echo $uscanout
+	done
+}
+
+function add_pkg() {
+	apt source $name
+	ln -s $name*/ ./$name
+}
+
+usc
