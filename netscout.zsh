65a256be1dc82a734322332a9be871f53920e715
Jack Laxson <jackjrabbit@gmail.com>
2020-12-17 18:30:40 -0500

    fixups clean syntax

--- netscout.zsh
+++ netscout.zsh
@@ -3,21 +3,21 @@
 PACKAGES=("signify-openbsd" "gnupg2")
 DOWNLOADS_FOLDER=$PWD
 
-function madison() {
+madison() {
 	#wget -nc "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.2.23.tar.bz2"
-	for pkg $PACKAGES 
+	for pkg in $PACKAGES
 		do newest=$(apt-cache madison $pkg | cut -d \| -f2 | xargs | cut -d " " -f 1)
 		echo $newest
-	done
+		done
 }
 
 
-function usc() {
+usc() {
 	# check if diskspace
 	avail=$(df --output=avail $PWD| tail -n1)
 	if [[ $avail -gt 500000000 ]] # 500 MB 
 	then
-		for pkg $PACKAGES
+		for pkg in $PACKAGES
 			do uscanout=$(cd $pkg && uscan 2> /dev/null) 
 			echo $uscanout
 		done
@@ -26,14 +26,13 @@ function usc() {
 	fi
 }
 
-function add_pkg(name) {
-	apt source $name
-	ln -s $name*/ ./$name
-}
+# add_pkg(name) {
+	# apt source $name
+	# ln -s $name*/ ./$name
+# }
 
-
-function download() {
-	for pkg $PACKAGES
+download() {
+	for pkg in $PACKAGES
 		do apt download $pkg
 	done
 }
